b7d832bb64f32a821483d8ef02b7b057359960b8
Adam Christie <fractos@gmail.com>
2016-12-14 18:35:35 +0000

    getting fedora3_karaf into a stable, installed state.

--- fedora3_karaf/scripts/fedora_camel_toolbox.sh
+++ fedora3_karaf/scripts/fedora_camel_toolbox.sh
@@ -22,17 +22,24 @@ sed -i 's|fcrepo.authPassword=$|fcrepo.authPassword=secret3|' /opt/karaf/etc/org
 if [ ! -f "/opt/karaf/etc/org.fcrepo.camel.indexing.solr.cfg" ]; then
    /opt/karaf/bin/client -u karaf -h localhost -a 8101 "feature:install fcrepo-indexing-solr"
 fi
+# Solr base url
 sed -i 's|solr.baseUrl=http://localhost:8983/solr/collection1|solr.baseUrl=http://search:8080/solr/collection1|' /opt/karaf/etc/org.fcrepo.camel.indexing.solr.cfg
+# Solr JMS broker url
+sed -i 's|jms.brokerUrl=tcp://localhost:61616|jms.brokerUrl=tcp://messaging:61616|' /opt/karaf/etc/org.fcrepo.camel.indexing.solr.cfg
 
 # Triplestore indexing
 if [ ! -f "/opt/karaf/etc/org.fcrepo.camel.indexing.triplestore.cfg" ]; then
    /opt/karaf/bin/client -u karaf -h localhost -a 8101 "feature:install fcrepo-indexing-triplestore"
 fi
+# Triplestore base url
+sed -i 's|triplestore.baseUrl=http://localhost:8080/fuseki/test/update|triplestore.baseUrl=http://triplestore:8080/fuseki/test/update|' /opt/karaf/etc/org.fcrepo.camel.indexing.triplestore.cfg
 
 # Audit service
 if [ ! -f "/opt/karaf/etc/org.fcrepo.camel.audit.cfg" ]; then
    /opt/karaf/bin/client -u karaf -h localhost -a 8101 "feature:install fcrepo-audit-triplestore"
 fi
+# Audit service triplestore base url
+sed -i 's|triplestore.baseUrl=http://localhost:8080/fuseki/test/update|triplestore.baseUrl=http://triplestore:8080/fuseki/test/update|' /opt/karaf/etc/org.fcrepo.camel.audit.cfg
 
 # Fixity service
 sleep 10
@@ -53,5 +60,5 @@ fi
 sed -i 's|rest.host=localhost$|rest.host=0.0.0.0|' /opt/karaf/etc/org.fcrepo.camel.reindexing.cfg
 
 sed -i 's|fcrepo.authUsername=$|fcrepo.authUsername=fedoraAdmin|' /opt/karaf/etc/org.fcrepo.camel.service.cfg
-sed -i 's|fcrepo.authPassword=$|fcrepo.authPassword=secret3|' /opt/karaf/etc/org.fcrepo.camel.service.cfg
+sed -i 's|fcrepo.authPassword=$|fcrepo.authPassword=secret|' /opt/karaf/etc/org.fcrepo.camel.service.cfg
 
--- fedora3_karaf/scripts/runall.sh
+++ fedora3_karaf/scripts/runall.sh
@@ -1,23 +1,40 @@
 #!/bin/bash
 
 cd $CATALINA_HOME
-catalina.sh run
+catalina.sh run &
+
 echo waiting for tomcat to start up for the first time
 sleep 45
 
 /opt/karaf/bin/start
+
+echo sleeping for 5 seconds
 sleep 5
+
 /opt/karaf/bin/client -f /root/karaf_service.script
 /opt/karaf/bin/stop
-ln -s /opt/karaf/bin/karaf-service /etc/init.d/
-update-rc.d karaf-service defaults 
+#ln -s /opt/karaf/bin/karaf-service /etc/init.d/
+#update-rc.d karaf-service defaults 
 sed -i "s|#org.ops4j.pax.url.mvn.localRepository=|org.ops4j.pax.url.mvn.localRepository=~/.m2/repository|" /opt/karaf/etc/org.ops4j.pax.url.mvn.cfg
 /opt/karaf/bin/start
+
+echo sleeping for 60 seconds
 sleep 60
+
 /root/fedora_camel_toolbox.sh
 /opt/karaf/bin/stop
+
+echo sleeping for 30 seconds
 sleep 30
+
 /opt/karaf/bin/start
+
+echo sleeping for 60 seconds
 sleep 60
+
 cd $CATALINA_HOME
+echo stopping tomcat...
+catalina.sh stop
+
+echo starting tomcat...
 catalina.sh run
